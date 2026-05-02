bfeacad7cfced2b6fd2867b5e6460b4541b9468d
Dat Phan <datphan2826@gmail.com>
2026-05-02 23:39:48 +0700

    js

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@ function plusSlides(n){
 
 function showSlides(n)
 {
-    var slides = document.getElementsByClassName("mySliders");
+    let slides = document.getElementsByClassName("mySliders");
     if(n > slides.length -1) slideIndex = 0;
     if(n < 0) slideIndex = slides.length - 1 ;
-    for (var i =0; i < slides.length ; i++)
+    for (let i =0; i < slides.length ; i++)
     {
         slides[i].style.display = "none";
     }
